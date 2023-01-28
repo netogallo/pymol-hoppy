09588ef522a9e1347222aae85b5e9f64d0755193
Ernesto Rodriguez <neto@netowork.me>
2023-01-28 18:17:07 +0000

    Function to derive a record from a cpp Class.

--- pymol-hoppy-extra/src/Data/Pymol/Extra.hs
+++ pymol-hoppy-extra/src/Data/Pymol/Extra.hs
@@ -2,18 +2,21 @@
 module Data.Pymol.Extra where
 
 import           Foreign (IntPtr(..), Ptr, intPtrToPtr)
+import           Foreign.Hoppy.Abstractions.Record (asRecord)
+import           Foreign.Hoppy.Runtime (CppPtr)
+import           Foreign.Pymol.Layer2.AtomInfo (classAtomInfo)
 import           Foreign.Storable (Storable(peek))
 import           Data.MonadicStreamFunction.Core (constM)
 import           Data.Pymol.Layer3.AtomIterators (SeleAtomIterator(..), getAtm, next)
+import           Data.Pymol.Layer2.AtomInfo (anisou_get)
+import Data.Pymol.Layer2.AtomInfo (AtomInfoType)
 
-import           Data.Pymol.Extra.TemplateHaskell (simple)
-
-$(simple ''SeleAtomIterator)
+$(asRecord classAtomInfo)
 
 foreign export ccall test :: IntPtr -> IO Int
 
-value :: SeleAtomIterator -> Kaiser
-value = Kaiser
+value :: Float -> AtomInfoTypeRec
+value = AtomInfoTypeRec
 
 test :: IntPtr -> IO Int
 test (SeleAtomIterator . intPtrToPtr -> it) = do

430b2df1a168d353687294b7b59552aa9ab4d28e
Ernesto Rodriguez <neto@netowork.me>
2023-01-28 21:38:15 +0000

    Proof of concept converter generator.

--- pymol-hoppy-extra/src/Data/Pymol/Extra.hs
+++ pymol-hoppy-extra/src/Data/Pymol/Extra.hs
@@ -1,22 +1,23 @@
-{-# LANGUAGE TemplateHaskell, ViewPatterns #-}
+{-# LANGUAGE MonoLocalBinds, TemplateHaskell, ViewPatterns #-}
 module Data.Pymol.Extra where
 
 import           Foreign (IntPtr(..), Ptr, intPtrToPtr)
-import           Foreign.Hoppy.Abstractions.Record (asRecord)
+import           Foreign.Hoppy.Abstractions.Record
 import           Foreign.Hoppy.Runtime (CppPtr)
 import           Foreign.Pymol.Layer2.AtomInfo (classAtomInfo)
 import           Foreign.Storable (Storable(peek))
 import           Data.MonadicStreamFunction.Core (constM)
 import           Data.Pymol.Layer3.AtomIterators (SeleAtomIterator(..), getAtm, next)
-import           Data.Pymol.Layer2.AtomInfo (anisou_get)
-import Data.Pymol.Layer2.AtomInfo (AtomInfoType)
+import           Data.Pymol.Layer2.AtomInfo (AtomInfoType, AtomInfoTypeValue, anisou_get)
 
 $(asRecord classAtomInfo)
-
 foreign export ccall test :: IntPtr -> IO Int
 
 value :: Float -> AtomInfoTypeRec
 value = AtomInfoTypeRec
+
+value' :: (AtomInfoTypeValue arg'1) => arg'1 -> IO AtomInfoTypeRec
+value' = convert_AtomInfoTypeRec
 
 test :: IntPtr -> IO Int
 test (SeleAtomIterator . intPtrToPtr -> it) = do
